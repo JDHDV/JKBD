5110083d35521ebc962746675fd59b1b323e3431
JDHDV <1073991028@qq.com>
2017-06-28 13:11:01 +0800

    闪屏2秒跳转

--- app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         cdt.start();
     }
-    CountDownTimer cdt=new CountDownTimer(3000,1000) {
+    CountDownTimer cdt=new CountDownTimer(2000,1000) {
         @Override
         public void onTick(long l) {
 
