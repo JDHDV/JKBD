870697750e789ba9fb51a8e522e0975c3fa1b8c9
JDHDV <1073991028@qq.com>
2017-06-29 16:55:42 +0800

    抓取数据

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.administrator.jkbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.ExamInfo;
+import com.example.administrator.jkbd.util.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void  Test(View view){
+        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(uri).targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
 
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
 
