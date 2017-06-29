56bb7ffdb6ab99c285685677703d61d3e4edaf13
JDHDV <1073991028@qq.com>
2017-06-29 15:10:07 +0800

    添加随机测试的点击事件

--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.administrator.jkbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
     }
+
+    public  void  Test(View view){
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+
+    public void Exit(){
+        finish();
+    }
+
+
 }
