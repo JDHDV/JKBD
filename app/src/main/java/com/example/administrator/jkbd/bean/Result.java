53bec0a41d2027627026654aed329200ebfac291
JDHDV <1073991028@qq.com>
2017-06-29 09:43:15 +0800

    修改完善Result实体类

--- app/src/main/java/com/example/administrator/jkbd/bean/Result.java
+++ app/src/main/java/com/example/administrator/jkbd/bean/Result.java
@@ -13,7 +13,7 @@ public class Result {
 
     private int error_code;
     private String reason;
-    private List<ResultBean> result;
+    private List<Question> result;
 
     public int getError_code() {
         return error_code;
@@ -31,107 +31,11 @@ public class Result {
         this.reason = reason;
     }
 
-    public List<ResultBean> getResult() {
+    public List<Question> getResult() {
         return result;
     }
 
-    public void setResult(List<ResultBean> result) {
+    public void setResult(List<Question> result) {
         this.result = result;
-    }
-
-    public static class ResultBean {
-        /**
-         * id : 16
-         * question : 这个标志是何含义？
-         * answer : 4
-         * item1 : 靠道路左侧停车
-         * item2 : 左侧是下坡路段
-         * item3 : 只准向左转弯
-         * item4 : 靠左侧道路行驶
-         * explains : 靠左侧道路行驶：表示只准一切车辆靠左侧道路行驶。此标志设在车辆必须靠左侧行驶的路口以前适当位置。
-         * url : http://images.juheapi.com/jztk/c1c2subject1/16.jpg
-         */
-
-        private int id;
-        private String question;
-        private String answer;
-        private String item1;
-        private String item2;
-        private String item3;
-        private String item4;
-        private String explains;
-        private String url;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getQuestion() {
-            return question;
-        }
-
-        public void setQuestion(String question) {
-            this.question = question;
-        }
-
-        public String getAnswer() {
-            return answer;
-        }
-
-        public void setAnswer(String answer) {
-            this.answer = answer;
-        }
-
-        public String getItem1() {
-            return item1;
-        }
-
-        public void setItem1(String item1) {
-            this.item1 = item1;
-        }
-
-        public String getItem2() {
-            return item2;
-        }
-
-        public void setItem2(String item2) {
-            this.item2 = item2;
-        }
-
-        public String getItem3() {
-            return item3;
-        }
-
-        public void setItem3(String item3) {
-            this.item3 = item3;
-        }
-
-        public String getItem4() {
-            return item4;
-        }
-
-        public void setItem4(String item4) {
-            this.item4 = item4;
-        }
-
-        public String getExplains() {
-            return explains;
-        }
-
-        public void setExplains(String explains) {
-            this.explains = explains;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
     }
 }
